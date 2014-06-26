14cdb335ac1c3da432ff34d5c324b1bd19f3cb6c
David <davidrgreenhalgh@gmail.com>
2014-06-26 17:40:33 -0400

    Cleaned up more autogenned code

--- app/src/main/java/com/bignerdranch/slidingpuzzle/PuzzleFragment.java
+++ app/src/main/java/com/bignerdranch/slidingpuzzle/PuzzleFragment.java
@@ -10,8 +10,7 @@ import android.view.ViewGroup;
 
 public class PuzzleFragment extends Fragment {
 
-    // TODO: Rename and change types and number of parameters
-    public static PuzzleFragment newInstance(String param1, String param2) {
+    public static PuzzleFragment newInstance() {
         PuzzleFragment fragment = new PuzzleFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
@@ -31,30 +30,5 @@ public class PuzzleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_puzzle, container, false);
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 }
