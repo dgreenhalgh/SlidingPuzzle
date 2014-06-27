37582beb27afffda8d38398a7c4504fe8d9ff2e0
David <davidrgreenhalgh@gmail.com>
2014-06-27 11:18:44 -0400

    Added RecyclerView, still not quite working.

--- app/src/main/java/com/bignerdranch/slidingpuzzle/PuzzleFragment.java
+++ app/src/main/java/com/bignerdranch/slidingpuzzle/PuzzleFragment.java
@@ -1,14 +1,20 @@
 package com.bignerdranch.slidingpuzzle;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.support.v7.widget.RecyclerView;
 
 public class PuzzleFragment extends Fragment {
+
+    private Button mNextButton;
+
+    private RecyclerView mPuzzleGridRecyclerView;
+    private RecyclerView.Adapter mPuzzleGridAdapter;
+    private RecyclerView.LayoutManager mPuzzleGridLayoutManager;
 
     public static PuzzleFragment newInstance() {
         PuzzleFragment fragment = new PuzzleFragment();
@@ -28,7 +34,18 @@ public class PuzzleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_puzzle, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_puzzle, container, false);
+
+        mPuzzleGridRecyclerView = (RecyclerView)view.findViewById(R.id.puzzle_grid_recyclerView);
+        mPuzzleGridRecyclerView.setHasFixedSize(true);
+
+        mPuzzleGridLayoutManager = new PuzzleGridLayoutManager();
+        mPuzzleGridRecyclerView.setLayoutManager(mPuzzleGridLayoutManager);
+
+        mPuzzleGridAdapter = new PuzzleGridAdapter();
+        mPuzzleGridRecyclerView.setAdapter(mPuzzleGridAdapter);
+
+        return view;
     }
 }
